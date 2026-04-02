1b32e937755242bf654472b448cb4f79f52ebc87
Shivendra Raghav <shivendra.raghav@activecyber.com>
2026-04-01 23:50:24 -0500

    Config Fix

--- actions/vitest.config.ts
+++ actions/vitest.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    typecheck: {
+      tsconfig: './tsconfig.test.json',
+    },
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
